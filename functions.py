a45f2c80625e7efd5a40a89c3aec08408b9bcae8
Joe Manlove <33290590+joemanlove@users.noreply.github.com>
2020-09-01 10:15:19 -0700

    PEP 8 fixes

--- functions.py
+++ functions.py
@@ -6,17 +6,19 @@ from discord import FFmpegPCMAudio, PCMVolumeTransformer
 # set filepath for decoder
 ffmpeg_path = 'ffmpeg/bin/ffmpeg'
 
+
 class Song:
     # constructor, also sets path
-    def __init__(self,_artist,_album,_title):
+    def __init__(self, _artist, _album, _title):
         self.artist = _artist
         self.album = _album
         self.title = _title
         self.path = f'music/{self.artist}/{self.album}/{self.title}'
-    
+
     # display method, returns string
     def displayTitle(self):
-       return f'Now playing:\n{self.title}\nby -> {self.artist}\non the album -> {self.album}'
+        return f'Now playing:\n{self.title}\nby -> {self.artist}\non the album -> {self.album}'
+
 
 def populateSongList():
     songList = []
@@ -31,10 +33,11 @@ def populateSongList():
         print('Warning empty Song Library.\n Check your file structure and terminal directory.')
     return songList
 
+
 # use this function for creating an audioSource from an mp3 file
 # from https://github.com/elibroftw/discord-bot/blob/master/bot.py
-def create_audio_source(music_filepath,start_at=0.0):
+def create_audio_source(music_filepath, start_at=0.0):
     audio_source = FFmpegPCMAudio(music_filepath, executable=ffmpeg_path, options='-vn -b:a 128k')
     audio_source = PCMVolumeTransformer(audio_source)
-    #audio_source.volume = guild_data['volume']
+    # audio_source.volume = guild_data['volume']
     return audio_source
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # settings should have prefix and token variables
-from settings import *
+from settings import prefix, token
 # from functions import populateSongList
 from functions import Song, populateSongList, create_audio_source
 
@@ -26,6 +26,7 @@ bot = discord.Client()
 # set command prefix
 bot = commands.Bot(command_prefix=prefix)
 
+
 # extend the VoiceClient class to include some more attributes
 class extendedVoiceClient(discord.VoiceClient):
     def setTextChannel(self, channel):
@@ -40,16 +41,19 @@ print('Initializing Song Library')
 songs = populateSongList()
 print(f'Song Library Initialized with {len(songs)} items.')
 
+
 # if the bot is ready print that out.
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     bot.loop.create_task(status_task())
 
+
 # ping command for testing purposes
-@bot.command(name = 'ping')
+@bot.command(name='ping')
 async def ping(ctx):
     await ctx.send('Pong!')
+
 
 async def setUpVoiceClient(ctx):
     # set vc to the voiceclient out of the dictionary, if there isn't one this will be 'none'
@@ -66,7 +70,7 @@ async def setUpVoiceClient(ctx):
         return None
     # if there isn't a client in the dictionary
     if not vc:
-        #connect to the users channel and store that value in the dict.
+        # connect to the users channel and store that value in the dict.
         vc = await channel.connect()
         voiceClientDictionary[ctx.guild.id] = vc
         # this is here to force an await after altering the dict.
@@ -74,7 +78,7 @@ async def setUpVoiceClient(ctx):
     # if the user isn't in the channel that the bot is, move the bot.
     elif vc.channel != channel:
         await vc.move_to(channel)
-    # typecast the VoiceClient into an extendedVoiceClient    
+    # typecast the VoiceClient into an extendedVoiceClient
     vc.__class__ = extendedVoiceClient
     vc.setTextChannel(ctx.channel)
     vc.setAutoPlay(False)
@@ -82,13 +86,13 @@ async def setUpVoiceClient(ctx):
 
 
 # join command to summon the bot to the voicechannel the user is in
-@bot.command(name = 'join')
+@bot.command(name='join')
 async def join(ctx):
     await setUpVoiceClient(ctx)
 
 
 # disconnect command
-@bot.command(name = 'leave', aliases = ['disconnect','go','goaway'])
+@bot.command(name='leave', aliases=['disconnect', 'go', 'goaway'])
 async def leave(ctx):
     # set vc to the voiceclient out of the dictionary, if there isn't one this will be 'none'
     vc = voiceClientDictionary.get(ctx.guild.id)
@@ -100,36 +104,39 @@ async def leave(ctx):
     else:
         await ctx.send('I am not connected to a voice channel in this server.')
 
+
 # play an audioSource
-@bot.command(name = 'play', aliases = ['skip','ff'])
+@bot.command(name='play', aliases=['skip', 'ff'])
 async def play(ctx):
     # get a voiceClient up and running or fetch the existing one
     vc = await setUpVoiceClient(ctx)
     if vc.is_playing():
         vc.stop()
     # pick a random song and play it.
-    song = choice(songs)  
+    song = choice(songs)
     source = create_audio_source(song.path)
     vc.play(source)
     await ctx.send(song.displayTitle())
 
+
 def playRandomSong(vc):
-    song = choice(songs)   
+    song = choice(songs)
     source = create_audio_source(song.path)
     vc.play(source)
     return song.displayTitle()
 
-@bot.command(name = 'autoplay')
+
+@bot.command(name='autoplay')
 async def autoplay(ctx):
     # get a voiceClient up and running or fetch the existing one
     vc = await setUpVoiceClient(ctx)
     vc.setAutoPlay(True)
-    
 
-@bot.command(name = 'source')
+
+@bot.command(name='source')
 async def source(ctx):
     # read in the source file
-    f = open('main.py','r')
+    f = open('main.py', 'r')
     contents = f.read()
     f.close()
 
@@ -146,7 +153,9 @@ async def source(ctx):
         # these kimda jackup the formatting, which is a little funny.
         await ctx.send('```' + chunk + '```')
 
-# timer code from https://stackoverflow.com/questions/46267705/making-a-discord-bot-change-playing-status-every-10-seconds
+
+# timer code from
+# https://stackoverflow.com/questions/46267705/making-a-discord-bot-change-playing-status-every-10-seconds
 async def status_task():
     while True:
         for guildId in voiceClientDictionary.keys():
@@ -155,7 +164,7 @@ async def status_task():
                 if not vc.is_playing():
                     st = playRandomSong(vc)
                     await vc.textChannel.send(st)
-                
+
         await asyncio.sleep(10)
 
 
